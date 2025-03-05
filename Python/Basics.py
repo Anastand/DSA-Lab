dd22a1aac33638195ed607109893e0bb371c387c
Aryan_Bhardwaj <bhardwaj03aryan@gmail.com>
2025-03-05 14:31:49 +0530

    learning basics

---
+++ Python/Basics.py
@@ -0,0 +1,28 @@
+# Day-01 -> march 5,25
+
+## topic -> variables , type-cast , 
+
+name = "Aryan"
+food = "pizza"
+
+# print("my name is " + name)
+# # the above is simple way of adding variable with string
+# print(f"my name is still{ name}")
+# # this is called fstring 
+
+# typecsating -> converting one type of variable to another
+# name = "aryan"
+# age = 111
+# gpa = 2.4
+
+# print(type(gpa))
+# gpa = int(gpa)
+# print(gpa)
+
+# user input ->input()
+
+# namr = int(input("what is your namme / age : "))
+# print(type(namr))
+# print(f"yo {namr}")
+
+# 
