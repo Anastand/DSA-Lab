6fee20b4da52f4efc4220a6deb471e8a0f2fab5e
Aryan_Bhardwaj <bhardwaj03aryan@gmail.com>
2025-03-05 14:57:48 +0530

    done till if confitions

--- Python/Basics.py
+++ Python/Basics.py
@@ -1,14 +1,14 @@
 # Day-01 -> march 5,25
 
-## topic -> variables , type-cast , 
+## topic -> variables , type-cast ,
 
-name = "Aryan"
-food = "pizza"
+# name = "Aryan"
+# food = "pizza"
 
 # print("my name is " + name)
 # # the above is simple way of adding variable with string
 # print(f"my name is still{ name}")
-# # this is called fstring 
+# # this is called fstring
 
 # typecsating -> converting one type of variable to another
 # name = "aryan"
@@ -25,4 +25,15 @@ food = "pizza"
 # print(type(namr))
 # print(f"yo {namr}")
 
-# 
+# conditions
+# if blocks
+age = int(input("Please enter your age: "))
+
+if age >= 80:
+    print("You should rest up.")
+elif age >= 18:
+    print(f"Congratulations on being {age}, let's get you driving.")
+else:
+    print("You are still a kid.")
+
+# continue from  https://youtu.be/ix9cRaBkVe0?list=PLZPZq0r_RZON03iKBjYOsOKr1-TD7z2lH&t=4468 ==> 1hr14min
